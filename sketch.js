88f0f1d3bd9768036f4143a8661ab67851729008
Graham Stodolski <stodolski101@gmail.com>
2023-04-11 15:36:33 -0400

    Change control to right wrist

--- sketch.js
+++ sketch.js
@@ -14,14 +14,14 @@ var frames = {
     var url = "ws://" + host + "/frames";
     frames.socket = new WebSocket(url);
     frames.socket.onmessage = function (event) {
-      var command = frames.get_left_wrist_command(JSON.parse(event.data));
+      var command = frames.get_right_wrist_command(JSON.parse(event.data));
       if (command !== null) {
         sendWristCommand(command);
       }
     }
   },
 
-  get_left_wrist_command: function (frame) {
+  get_right_wrist_command: function (frame) {
     var command = null;
     if (frame.people.length < 1) {
       return command;
@@ -31,24 +31,24 @@ var frames = {
     var pelvis_x = frame.people[0].joints[0].position.x;
     var pelvis_y = frame.people[0].joints[0].position.y;
     var pelvis_z = frame.people[0].joints[0].position.z;
-    var left_wrist_x = (frame.people[0].joints[7].position.x - pelvis_x) * -1;
-    var left_wrist_y = (frame.people[0].joints[7].position.y - pelvis_y) * -1;
-    var left_wrist_z = (frame.people[0].joints[7].position.z - pelvis_z) * -1;
+    var right_wrist_x = (frame.people[0].joints[14].position.x - pelvis_x) * -1;
+    var right_wrist_y = (frame.people[0].joints[14].position.y - pelvis_y) * -1;
+    var right_wrist_z = (frame.people[0].joints[14].position.z - pelvis_z) * -1;
 
-    if (left_wrist_z < 100) {
+    if (right_wrist_z < 100) {
       return command;
     }
 
-    if (left_wrist_x < 200 && left_wrist_x > -200) {
-      if (left_wrist_y > 500) {
+    if (right_wrist_x < 200 && right_wrist_x > -200) {
+      if (right_wrist_y > 500) {
         command = 73; // UP
-      } else if (left_wrist_y < 100) {
+      } else if (right_wrist_y < 100) {
         command = 75; // DOWN
       }
-    } else if (left_wrist_y < 500 && left_wrist_y > 100) {
-      if (left_wrist_x > 200) {
+    } else if (right_wrist_y < 500 && right_wrist_y > 100) {
+      if (right_wrist_x > 200) {
         command = 76; // RIGHT
-      } else if (left_wrist_x < -200) {
+      } else if (right_wrist_x < -200) {
         command = 74; // LEFT
       }
     }
