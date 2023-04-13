4e0695bd104ee8a8959e4dc25a3cb08e649deb0b
Graham Stodolski <stodolski101@gmail.com>
2023-04-13 13:48:29 -0400

    Implement quit gesture

--- sketch.js
+++ sketch.js
@@ -21,36 +21,31 @@ var frames = {
     }
   },
 
-  get_right_wrist_command: function (frame) {
+  is_raising_hands: function (frame) {
     var command = null;
     if (frame.people.length < 1) {
       return command;
     }
 
     // Normalize by subtracting the root (pelvis) joint coordinates
-    var pelvis_x = frame.people[0].joints[0].position.x;
-    var pelvis_y = frame.people[0].joints[0].position.y;
-    var pelvis_z = frame.people[0].joints[0].position.z;
-    var right_wrist_x = (frame.people[0].joints[14].position.x - pelvis_x) * -1;
-    var right_wrist_y = (frame.people[0].joints[14].position.y - pelvis_y) * -1;
-    var right_wrist_z = (frame.people[0].joints[14].position.z - pelvis_z) * -1;
+    var neck_x = frame.people[0].joints[3].position.x;
+    var neck_y = frame.people[0].joints[3].position.y;
+    var neck_z = frame.people[0].joints[3].position.z;
+    var left_wrist_x = (frame.people[0].joints[7].position.x - neck_x) * -1;
+    var left_wrist_y = (frame.people[0].joints[7].position.y - neck_y) * -1;
+    var left_wrist_z = (frame.people[0].joints[7].position.z - neck_z) * -1;
+    var right_wrist_x = (frame.people[0].joints[14].position.x - neck_x) * -1;
+    var right_wrist_y = (frame.people[0].joints[14].position.y - neck_y) * -1;
+    var right_wrist_z = (frame.people[0].joints[14].position.z - neck_z) * -1;
 
-    if (right_wrist_z < 100) {
+    if (left_wrist_z && right_wrist_z < 100) {
       return command;
     }
 
-    if (right_wrist_x < 200 && right_wrist_x > -200) {
-      if (right_wrist_y > 500) {
-        command = 73; // UP
-      } else if (right_wrist_y < 100) {
-        command = 75; // DOWN
-      }
-    } else if (right_wrist_y < 500 && right_wrist_y > 100) {
-      if (right_wrist_x > 200) {
-        command = 76; // RIGHT
-      } else if (right_wrist_x < -200) {
-        command = 74; // LEFT
-      }
+    if (right_wrist_y > 500 && left_wrist_y > 500) {
+      command = 100; // QUIT
+    } else {
+      command = 1; // NOT QUIT
     }
     return command;
   },
